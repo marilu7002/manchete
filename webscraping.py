01020ea065a568dbba56169db71f170c9f527fd4
maria luiza <148582678+marilu7002@users.noreply.github.com>
2023-11-17 16:28:31 -0300

    Update and rename README.md to webscraping.py

--- webscraping.py
+++ webscraping.py
@@ -1,6 +1,7 @@
 # manchete
-Código sobre  Web Scraping
+Código sobre Web Scraping. 
+Separando manchetes em um site de notícias;
 Este é uma atividade onde estamos aprendendo sobre Web Scraping;
-Somos alunas do 1° E do curso Técnico Integrado em informática do Instituto Federal de Educação, Ciência e Tecnologia Sudeste de Minas Gerais Campus Rio Pomba;
+Somos alunas do 1° E do curso Técnico em informática do Instituto Federal de Educação, Ciência e Tecnologia Sudeste de Minas Gerais Campus Rio Pomba;
 Esta atividade foi desenvolvida na disciplina de introdução a Algoritimos;
 As desenvolvedoras são: Maria Luiza Ferreira da Silva e Isabella de Jesus Silva.
